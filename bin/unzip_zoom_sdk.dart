0fe40b1ee2a4ee83ab6f23582d5dbff499ea804d
Nikhil Ramesh <thenameisnikil@gmail.com>
2024-02-05 21:38:06 +0530

    update unzip file

--- bin/unzip_zoom_sdk.dart
+++ bin/unzip_zoom_sdk.dart
@@ -1,7 +1,6 @@
 import 'dart:core';
 import 'dart:io';
 import 'dart:convert';
-//import 'package:dio/dio.dart';
 
 void main(List<String> args) async {
   var location = Platform.script.toString();
@@ -13,20 +12,18 @@ void main(List<String> args) async {
     var scriptDir = sd.join(Platform.pathSeparator);
     var packageConfigPath = [scriptDir, '..', '..', '..', 'package_config.json']
         .join(Platform.pathSeparator);
-    // print(packageConfigPath);
     var jsonString = File(packageConfigPath).readAsStringSync();
-    // print(jsonString);
     Map<String, dynamic> packages = jsonDecode(jsonString);
     var packageList = packages["packages"];
     String? zoomFileUri;
     for (var package in packageList) {
-      if (package["name"] == "gr_zoom") {
+      if (package["name"] == "flutter_zoom_sdk") {
         zoomFileUri = package["rootUri"];
         break;
       }
     }
     if (zoomFileUri == null) {
-      print("gr_zoom package not found!");
+      print("flutter_zoom_sdk package not found!");
       return;
     }
     location = zoomFileUri;
@@ -36,31 +33,11 @@ void main(List<String> args) async {
   } else {
     location = location.replaceFirst("file://", "");
   }
-  if (!isNewFlutter)
+  if (!isNewFlutter) {
     location = location.replaceFirst("/bin/unzip_zoom_sdk.dart", "");
-  // var filename =
-  //     location + '/ios-sdk/MobileRTC${(args.length == 0) ? "" : "-dev"}.zip';
+  }
 
   await checkAndDownloadSDK(location);
-  // print('Decompressing ' + filename);
-
-  // final bytes = File(filename).readAsBytesSync();
-
-  // final archive = ZipDecoder().decodeBytes(bytes);
-
-  // var current = new File(location + '/ios/MobileRTC.framework/MobileRTC');
-  // var exist = await current.exists();
-  // if (exist) current.deleteSync();
-
-  // for (final file in archive) {
-  //   final filename = file.name;
-  //   if (file.isFile) {
-  //     final data = file.content as List<int>;
-  //     File(location + '/ios/MobileRTC.framework/' + filename)
-  //       ..createSync(recursive: true)
-  //       ..writeAsBytesSync(data);
-  //   }
-  // }
 
   print('Complete');
 }
@@ -72,8 +49,7 @@ Future<void> checkAndDownloadSDK(String location) async {
 
   if (!exists) {
     await downloadFile(
-        Uri.parse('https://www.dropbox.com/
-scl/fi/rotru7h0hzfb57nk4krsy/MobileRTC?rlkey=w2i5c45d1nuzlvo77iukg5ed6&dl=1'), // updated the version from 5.13.5
+        Uri.parse('https://www.dropbox.com/scl/fi/rotru7h0hzfb57nk4krsy/MobileRTC?rlkey=w2i5c45d1nuzlvo77iukg5ed6&dl=1'),
         iosSDKFile);
   }
 
@@ -83,8 +59,7 @@ scl/fi/rotru7h0hzfb57nk4krsy/MobileRTC?rlkey=w2i5c45d1nuzlvo77iukg5ed6&dl=1'), /
 
   if (!exists) {
     await downloadFile(
-        Uri.parse('https://www.dropbox.com/
-scl/fi/wkphhnuz9ezwt9pgx7o6n/MobileRTC?rlkey=01dsox5ghw5g0e1e95cn2vio3&dl=1'),
+        Uri.parse('https://www.dropbox.com/scl/fi/wkphhnuz9ezwt9pgx7o6n/MobileRTC?rlkey=01dsox5ghw5g0e1e95cn2vio3&dl=1'),
         iosSimulateSDKFile);
   }
 
@@ -93,7 +68,7 @@ scl/fi/wkphhnuz9ezwt9pgx7o6n/MobileRTC?rlkey=01dsox5ghw5g0e1e95cn2vio3&dl=1'),
   if (!exists) {
     await downloadFile(
         Uri.parse(
-            'https://com21-static.s3.sa-east-1.amazonaws.com/zoom/android/5.13.5/commonlib.aar?dl=1'),
+            'https://www.dropbox.com/scl/fi/4tty42n51byo46dis9gys/commonlib.aar?rlkey=b6s30n4htpht13gv93w14tm87&dl=1'),
         androidCommonLibFile);
   }
   var androidRTCLibFile = location + '/android/libs/mobilertc.aar';
@@ -101,7 +76,7 @@ scl/fi/wkphhnuz9ezwt9pgx7o6n/MobileRTC?rlkey=01dsox5ghw5g0e1e95cn2vio3&dl=1'),
   if (!exists) {
     await downloadFile(
         Uri.parse(
-            'https://com21-static.s3.sa-east-1.amazonaws.com/zoom/android/5.13.5/mobilertc.aar?dl=1'),
+            'https://www.dropbox.com/scl/fi/kyo35tu2ikktep4wwcjjd/mobilertc.aar?rlkey=s84h79nvmm57ng04i5qmywwss&dl=1'),
         androidRTCLibFile);
   }
 }
@@ -109,11 +84,7 @@ scl/fi/wkphhnuz9ezwt9pgx7o6n/MobileRTC?rlkey=01dsox5ghw5g0e1e95cn2vio3&dl=1'),
 Future<void> downloadFile(Uri uri, String savePath) async {
   print('Download ${uri.toString()} to $savePath');
   File destinationFile = await File(savePath).create(recursive: true);
-  // var dio = Dio();
-  // dio.options.connectTimeout = 1000000;
-  // dio.options.receiveTimeout = 1000000;
-  // dio.options.sendTimeout = 1000000;
-  // await dio.downloadUri(uri, savePath);
+
   final request = await HttpClient().getUrl(uri);
   final response = await request.close();
   await response.pipe(destinationFile.openWrite());
